67d7cb501ac3ec797abbf56f00a5c14d989cd63d
lejonmcgowan <lejonmcgowan@gmail.com>
2016-06-09 18:52:02 -0700

    getting smoke

--- index.js
+++ index.js
@@ -6,18 +6,30 @@ requirejs.config({
 });
 var cursors;
 var jumpButton, walkButton;
+var fullscreenToggleButton;
+
 var walkSpeed = 150, runSpeed = 450;
 var jumpTimer = 0;
 var sprite,floor, testSquare;
+var smoketrail;
+var nextFire;
 
 require(["Phaser"],
     function(Phaser)
     {
-        var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { create: create, render: render, update: update });
+        var game = new Phaser.Game(1280, 720, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, render: render, update: update });
 
+        function preload()
+        {
+            game.load.image("smoke","assets/smoke.png");
+        }
 
         function create()
         {
+            this.game.world.setBounds(0, 0, 3500, this.game.height);
+            game.scale.fullScreenScaleMode = Phaser.ScaleManager.EXACT_FIT;
+            this.game.stage.backgroundColor = "#4488AA";
+
             testSquare = game.add.graphics();
             testSquare.beginFill(0xff0000,1);
             testSquare.drawRect(0,0,100,100);
@@ -29,35 +41,87 @@ require(["Phaser"],
             sprite.body.collideWorldBounds = true;
             sprite.body.setSize(testSquare.width, testSquare.height);
 
+            //the camera will follow the player in the world
+            this.game.camera.follow(sprite);
+
+            smoketrail = game.add.group();
+            smoketrail.enableBody = true;
+            smoketrail.physicsBodyType = Phaser.Physics.ARCADE;
+
+            smoketrail.createMultiple(50,'smoke');
+            smoketrail.setAll('checkWorldBounds', true);
+            smoketrail.setAll('outOfBoundsKill', true);
+
             cursors = game.input.keyboard.createCursorKeys();
             jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
             walkButton = game.input.keyboard.addKey(Phaser.Keyboard.SHIFT);
+            fullscreenToggleButton = game.input.keyboard.addKey(Phaser.Keyboard.F);
 
+            sprite.body.gravity.y = 2000;
         }
 
         function render ()
         {
             //game.debug.geom(floor,'#00ffff');
             game.physics.arcade.overlap(sprite,floor,function(r,t){r.body.velocity.y = 0;});
-            sprite.body.gravity.y = 2000;
-
-            //game.debug.body(sprite);
         }
 
         function update()
         {
-            sprite.body.velocity.x -= 10;
-            sprite.body.velocity.x = Math.max(sprite.body.velocity.x,0);
-            if (cursors.left.isDown)
+            if(sprite.body.velocity.x < 0) {
+                sprite.body.velocity.x += 15;
+                sprite.body.velocity.x = Math.min(sprite.body.velocity.x,0);
+            }
+            else {
+                sprite.body.velocity.x -= 15;
+                sprite.body.velocity.x = Math.max(sprite.body.velocity.x,0);
+            }
+
+            if (cursors.left.isDown || game.input.keyboard.isDown(Phaser.Keyboard.A))
             {
                 sprite.body.velocity.x = walkButton.isDown ? -walkSpeed : -runSpeed;
             }
-            else if (cursors.right.isDown)
+            else if (cursors.right.isDown || game.input.keyboard.isDown(Phaser.Keyboard.D))
             {
                 sprite.body.velocity.x = walkButton.isDown ? walkSpeed : runSpeed;
             }
 
-            if (jumpButton.isDown && sprite.body.onFloor())
+            if ((jumpButton.isDown || cursors.up.isDown || game.input.keyboard.isDown(Phaser.Keyboard.W))
+                && sprite.body.onFloor())
                 sprite.body.velocity.y = -1000;
+
+            if(fullscreenToggleButton.isDown)
+            {
+                //figure out later
+
+            }
+
+            if(cursors.down.isDown || game.input.keyboard.isDown(Phaser.Keyboard.S))
+                sprite.body.gravity.y = 4500;
+            else
+                sprite.body.gravity.y = 2000;
+
+            //create smoke trail
+            if (game.input.mousePointer.isDown)
+            {
+                createSmoke();
+            }
         }
+        function createSmoke()
+        {
+            console.log("SMOKE at (" + game.input.worldX + "," + game.input.worldY + ")");
+
+            if (game.time.now > nextFire && smoketrail.countDead() > 0)
+            {
+                nextFire = game.time.now + fireRate;
+
+                var newPuff = smoketrail.getFirstDead();
+
+                newPuff.reset(game.input.worldX, game.input.worldY);
+
+                game.physics.arcade.moveToPointer(newPuff, 300);
+            }
+
+        }
+
     });
